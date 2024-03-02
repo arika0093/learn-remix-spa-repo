ce9c640ed358aa80454ecc507c83b203ebbdd836
Arika Ishinami <delete0093@gmail.com>
2024-03-02 02:39:20 +0000

    change lefthook

--- build/client/assets/tiny-invariant-CopsF_GD.js
+++ build/client/assets/tiny-invariant-CopsF_GD.js
@@ -1,5 +1,1 @@
-var a = "Invariant failed";
-function n(r, i) {
-	if (!r) throw new Error(a);
-}
-export { n as i };
+var a="Invariant failed";function n(r,i){if(!r)throw new Error(a)}export{n as i};
