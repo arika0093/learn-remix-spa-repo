dbc5f1eb9fc8c238085a2db539ad3025348c9d43
Arika Ishinami <delete0093@gmail.com>
2024-03-02 02:08:30 +0000

    change use linter eslint to biome

--- app/root.tsx
+++ app/root.tsx
@@ -1,43 +1,44 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 import "./globals.css";
+import React from "react";
 import { useAtomValue } from "jotai";
 import { darkModeAtom } from "./models/local";
 import { IndexPage } from "@/components/index-page";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const darkMode = useAtomValue(darkModeAtom);
+	const darkMode = useAtomValue(darkModeAtom);
 
-  return (
-    <html className={darkMode ? "dark" : ""} lang="ja">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html className={darkMode ? "dark" : ""} lang="ja">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return (
-    <IndexPage>
-      <Outlet />
-    </IndexPage>
-  )
+	return (
+		<IndexPage>
+			<Outlet />
+		</IndexPage>
+	);
 }
 
 export function HydrateFallback() {
-  return <p>Loading...</p>;
+	return <p>Loading...</p>;
 }
