b7c636e6739bb98be32e324ac9813d4551bc3257
Arika Ishinami <delete0093@gmail.com>
2024-03-02 06:38:45 +0000

    Update import paths for utils module

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/shadcn_utils";
 
 export interface InputProps
 	extends React.InputHTMLAttributes<HTMLInputElement> {}
